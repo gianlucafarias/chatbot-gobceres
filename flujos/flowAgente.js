9780f242683a894cf34c3450806a257eb60f3930
gianlucafarias <palmiergianluca@gmail.com>
2024-03-18 10:43:53 -0300

    version 1.1.2 fix 2

--- flujos/flowAgente.js
+++ flujos/flowAgente.js
@@ -9,12 +9,12 @@ const {
     
 } = require('@bot-whatsapp/bot')
 const flowAgente = addKeyword('PELIGRO', {sensitive: true})
-.addAnswer('Estamos creando una conexion con un agente local...')
+.addAnswer('Estamos creando una conexion con un agente local de ojos en alerta...')
 .addAction(async (ctx, {provider}) => {
     const nanoid = await import('nanoid')
     const ID_GROUP = nanoid.nanoid(5)
     const refProvider = await provider.getInstance()
-    await refProvider.groupCreate(`Guardia Local (${ID_GROUP})`,[
+    await refProvider.groupCreate(`Ojos en Alerta Reclamo:(${ID_GROUP})`,[
         `${ctx.from}@s.whatsapp.net`
     ])
 })
